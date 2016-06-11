5cc71bb0218d39e1623a45a9930753beb87aa81d
33Degrees <m.godfrey@33d.us>
2016-06-11 18:08:48 -0500

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wpui_postmeta.sql
+++ wp-content/uploads/revisr-backups/revisr_wpui_postmeta.sql
@@ -1,17 +1,4 @@
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 DROP TABLE IF EXISTS `wpui_postmeta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wpui_postmeta` (
   `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` bigint(20) unsigned NOT NULL DEFAULT '0',
@@ -21,20 +8,6 @@ CREATE TABLE `wpui_postmeta` (
   KEY `post_id` (`post_id`),
   KEY `meta_key` (`meta_key`(191))
 ) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 LOCK TABLES `wpui_postmeta` WRITE;
-/*!40000 ALTER TABLE `wpui_postmeta` DISABLE KEYS */;
-INSERT INTO `wpui_postmeta` VALUES (1,2,'_wp_page_template','default'),(2,4,'_menu_item_type','custom'),(3,4,'_menu_item_menu_item_parent','0'),(4,4,'_menu_item_object_id','4'),(5,4,'_menu_item_object','custom'),(6,4,'_menu_item_target',''),(7,4,'_menu_item_classes','a:1:{i:0;s:0:\"\";}'),(8,4,'_menu_item_xfn',''),(9,4,'_menu_item_url','http://vinehurst.com/'),(10,4,'_menu_item_orphaned','1465681189'),(11,5,'_menu_item_type','post_type'),(12,5,'_menu_item_menu_item_parent','0'),(13,5,'_menu_item_object_id','2'),(14,5,'_menu_item_object','page'),(15,5,'_menu_item_target',''),(16,5,'_menu_item_classes','a:1:{i:0;s:0:\"\";}'),(17,5,'_menu_item_xfn',''),(18,5,'_menu_item_url',''),(19,5,'_menu_item_orphaned','1465681189'),(20,6,'_menu_item_type','custom'),(21,6,'_menu_item_menu_item_parent','0'),(22,6,'_menu_item_object_id','6'),(23,6,'_menu_item_object','custom'),(24,6,'_menu_item_target',''),(25,6,'_menu_item_classes','a:1:{i:0;s:0:\"\";}'),(26,6,'_menu_item_xfn',''),(27,6,'_menu_item_url','http://vinehurst.com/'),(28,6,'_menu_item_orphaned','1465681434'),(29,7,'_menu_item_type','post_type'),(30,7,'_menu_item_menu_item_parent','0'),(31,7,'_menu_item_object_id','2'),(32,7,'_menu_item_object','page'),(33,7,'_menu_item_target',''),(34,7,'_menu_item_classes','a:1:{i:0;s:0:\"\";}'),(35,7,'_menu_item_xfn',''),(36,7,'_menu_item_url',''),(37,7,'_menu_item_orphaned','1465681434');
-/*!40000 ALTER TABLE `wpui_postmeta` ENABLE KEYS */;
+INSERT INTO `wpui_postmeta` VALUES ('1','2','_wp_page_template','default'), ('2','4','_menu_item_type','custom'), ('3','4','_menu_item_menu_item_parent','0'), ('4','4','_menu_item_object_id','4'), ('5','4','_menu_item_object','custom'), ('6','4','_menu_item_target',''), ('7','4','_menu_item_classes','a:1:{i:0;s:0:\"\";}'), ('8','4','_menu_item_xfn',''), ('9','4','_menu_item_url','http://vinehurst.com/'), ('10','4','_menu_item_orphaned','1465681189'), ('11','5','_menu_item_type','post_type'), ('12','5','_menu_item_menu_item_parent','0'), ('13','5','_menu_item_object_id','2'), ('14','5','_menu_item_object','page'), ('15','5','_menu_item_target',''), ('16','5','_menu_item_classes','a:1:{i:0;s:0:\"\";}'), ('17','5','_menu_item_xfn',''), ('18','5','_menu_item_url',''), ('19','5','_menu_item_orphaned','1465681189'), ('20','6','_menu_item_type','custom'), ('21','6','_menu_item_menu_item_parent','0'), ('22','6','_menu_item_object_id','6'), ('23','6','_menu_item_object','custom'), ('24','6','_menu_item_target',''), ('25','6','_menu_item_classes','a:1:{i:0;s:0:\"\";}'), ('26','6','_menu_item_xfn',''), ('27','6','_menu_item_url','http://vinehurst.com/'), ('28','6','_menu_item_orphaned','1465681434'), ('29','7','_menu_item_type','post_type'), ('30','7','_menu_item_menu_item_parent','0'), ('31','7','_menu_item_object_id','2'), ('32','7','_menu_item_object','page'), ('33','7','_menu_item_target',''), ('34','7','_menu_item_classes','a:1:{i:0;s:0:\"\";}'), ('35','7','_menu_item_xfn',''), ('36','7','_menu_item_url',''), ('37','7','_menu_item_orphaned','1465681434');
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- wp-content/uploads/revisr-backups/revisr_wpui_posts.sql
+++ wp-content/uploads/revisr-backups/revisr_wpui_posts.sql
@@ -1,17 +1,4 @@
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 DROP TABLE IF EXISTS `wpui_posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wpui_posts` (
   `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `post_author` bigint(20) unsigned NOT NULL DEFAULT '0',
@@ -42,20 +29,6 @@ CREATE TABLE `wpui_posts` (
   KEY `post_parent` (`post_parent`),
   KEY `post_author` (`post_author`)
 ) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 LOCK TABLES `wpui_posts` WRITE;
-/*!40000 ALTER TABLE `wpui_posts` DISABLE KEYS */;
-INSERT INTO `wpui_posts` VALUES (1,1,'2016-06-07 21:10:29','2016-06-07 21:10:29','Welcome to WordPress. This is your first post. Edit or delete it, then start writing!','Hello world!','','publish','open','open','','hello-world','','','2016-06-07 21:10:29','2016-06-07 21:10:29','',0,'http://vinehurst.com/wp/?p=1',0,'post','',1),(2,1,'2016-06-07 21:10:29','2016-06-07 21:10:29','This is an example page. It\'s different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:\n\n<blockquote>Hi there! I\'m a bike messenger by day, aspiring actor by night, and this is my website. I live in Los Angeles, have a great dog named Jack, and I like pi&#241;a coladas. (And gettin\' caught in the rain.)</blockquote>\n\n...or something like this:\n\n<blockquote>The XYZ Doohickey Company was founded in 1971, and has been providing quality doohickeys to the public ever since. Located in Gotham City, XYZ employs over 2,000 people and does all kinds of awesome things for the Gotham community.</blockquote>\n\nAs a new WordPress user, you should go to <a href=\"http://vinehurst.com/wp/wp-admin/\">your dashboard</a> to delete this page and create new pages for your content. Have fun!','Sample Page','','publish','closed','open','','sample-page','','','2016-06-07 21:10:29','2016-06-07 21:10:29','',0,'http://vinehurst.com/wp/?page_id=2',0,'page','',0),(3,1,'2016-06-07 21:19:48','0000-00-00 00:00:00','','Auto Draft','','auto-draft','open','open','','','','','2016-06-07 21:19:48','0000-00-00 00:00:00','',0,'http://vinehurst.com/?p=3',0,'post','',0),(4,1,'2016-06-11 21:39:49','0000-00-00 00:00:00','','Home','','draft','closed','closed','','','','','2016-06-11 21:39:49','0000-00-00 00:00:00','',0,'http://vinehurst.com/?p=4',1,'nav_menu_item','',0),(5,1,'2016-06-11 21:39:49','0000-00-00 00:00:00',' ','','','draft','closed','closed','','','','','2016-06-11 21:39:49','0000-00-00 00:00:00','',0,'http://vinehurst.com/?p=5',1,'nav_menu_item','',0),(6,1,'2016-06-11 21:43:54','0000-00-00 00:00:00','','Home','','draft','closed','closed','','','','','2016-06-11 21:43:54','0000-00-00 00:00:00','',0,'http://vinehurst.com/?p=6',1,'nav_menu_item','',0),(7,1,'2016-06-11 21:43:54','0000-00-00 00:00:00',' ','','','draft','closed','closed','','','','','2016-06-11 21:43:54','0000-00-00 00:00:00','',0,'http://vinehurst.com/?p=7',1,'nav_menu_item','',0);
-/*!40000 ALTER TABLE `wpui_posts` ENABLE KEYS */;
+INSERT INTO `wpui_posts` VALUES ('1','1','2016-06-07 21:10:29','2016-06-07 21:10:29','Welcome to WordPress. This is your first post. Edit or delete it, then start writing!','Hello world!','','publish','open','open','','hello-world','','','2016-06-07 21:10:29','2016-06-07 21:10:29','','0','http://vinehurst.com/wp/?p=1','0','post','','1'), ('2','1','2016-06-07 21:10:29','2016-06-07 21:10:29','This is an example page. It\'s different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:\n\n<blockquote>Hi there! I\'m a bike messenger by day, aspiring actor by night, and this is my website. I live in Los Angeles, have a great dog named Jack, and I like pi&#241;a coladas. (And gettin\' caught in the rain.)</blockquote>\n\n...or something like this:\n\n<blockquote>The XYZ Doohickey Company was founded in 1971, and has been providing quality doohickeys to the public ever since. Located in Gotham City, XYZ employs over 2,000 people and does all kinds of awesome things for the Gotham community.</blockquote>\n\nAs a new WordPress user, you should go to <a href=\"http://vinehurst.com/wp/wp-admin/\">your dashboard</a> to delete this page and create new pages for your content. Have fun!','Sample Page','','publish','closed','open','','sample-page','','','2016-06-07 21:10:29','2016-06-07 21:10:29','','0','http://vinehurst.com/wp/?page_id=2','0','page','','0'), ('3','1','2016-06-07 21:19:48','0000-00-00 00:00:00','','Auto Draft','','auto-draft','open','open','','','','','2016-06-07 21:19:48','0000-00-00 00:00:00','','0','http://vinehurst.com/?p=3','0','post','','0'), ('4','1','2016-06-11 21:39:49','0000-00-00 00:00:00','','Home','','draft','closed','closed','','','','','2016-06-11 21:39:49','0000-00-00 00:00:00','','0','http://vinehurst.com/?p=4','1','nav_menu_item','','0'), ('5','1','2016-06-11 21:39:49','0000-00-00 00:00:00',' ','','','draft','closed','closed','','','','','2016-06-11 21:39:49','0000-00-00 00:00:00','','0','http://vinehurst.com/?p=5','1','nav_menu_item','','0'), ('6','1','2016-06-11 21:43:54','0000-00-00 00:00:00','','Home','','draft','closed','closed','','','','','2016-06-11 21:43:54','0000-00-00 00:00:00','','0','http://vinehurst.com/?p=6','1','nav_menu_item','','0'), ('7','1','2016-06-11 21:43:54','0000-00-00 00:00:00',' ','','','draft','closed','closed','','','','','2016-06-11 21:43:54','0000-00-00 00:00:00','','0','http://vinehurst.com/?p=7','1','nav_menu_item','','0');
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
