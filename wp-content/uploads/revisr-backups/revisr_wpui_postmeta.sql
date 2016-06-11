59d80c5a9abf47b1fdd8661a93fe02ab3429a544
33Degrees <m.godfrey@33d.us>
2016-06-11 16:45:27 -0500

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_wpui_postmeta.sql
+++ wp-content/uploads/revisr-backups/revisr_wpui_postmeta.sql
@@ -20,12 +20,12 @@ CREATE TABLE `wpui_postmeta` (
   PRIMARY KEY (`meta_id`),
   KEY `post_id` (`post_id`),
   KEY `meta_key` (`meta_key`(191))
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wpui_postmeta` WRITE;
 /*!40000 ALTER TABLE `wpui_postmeta` DISABLE KEYS */;
-INSERT INTO `wpui_postmeta` VALUES (1,2,'_wp_page_template','default');
+INSERT INTO `wpui_postmeta` VALUES (1,2,'_wp_page_template','default'),(2,4,'_menu_item_type','custom'),(3,4,'_menu_item_menu_item_parent','0'),(4,4,'_menu_item_object_id','4'),(5,4,'_menu_item_object','custom'),(6,4,'_menu_item_target',''),(7,4,'_menu_item_classes','a:1:{i:0;s:0:\"\";}'),(8,4,'_menu_item_xfn',''),(9,4,'_menu_item_url','http://vinehurst.com/'),(10,4,'_menu_item_orphaned','1465681189'),(11,5,'_menu_item_type','post_type'),(12,5,'_menu_item_menu_item_parent','0'),(13,5,'_menu_item_object_id','2'),(14,5,'_menu_item_object','page'),(15,5,'_menu_item_target',''),(16,5,'_menu_item_classes','a:1:{i:0;s:0:\"\";}'),(17,5,'_menu_item_xfn',''),(18,5,'_menu_item_url',''),(19,5,'_menu_item_orphaned','1465681189'),(20,6,'_menu_item_type','custom'),(21,6,'_menu_item_menu_item_parent','0'),(22,6,'_menu_item_object_id','6'),(23,6,'_menu_item_object','custom'),(24,6,'_menu_item_target',''),(25,6,'_menu_item_classes','a:1:{i:0;s:0:\"\";}'),(26,6,'_menu_item_xfn',''),(27,6,'_menu_item_url','http://vinehurst.com/'),(28,6,'_menu_item_orphaned','1465681434'),(29,7,'_menu_item_type','post_type'),(30,7,'_menu_item_menu_item_parent','0'),(31,7,'_menu_item_object_id','2'),(32,7,'_menu_item_object','page'),(33,7,'_menu_item_target',''),(34,7,'_menu_item_classes','a:1:{i:0;s:0:\"\";}'),(35,7,'_menu_item_xfn',''),(36,7,'_menu_item_url',''),(37,7,'_menu_item_orphaned','1465681434');
 /*!40000 ALTER TABLE `wpui_postmeta` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
--- wp-content/uploads/revisr-backups/revisr_wpui_usermeta.sql
+++ wp-content/uploads/revisr-backups/revisr_wpui_usermeta.sql
@@ -20,12 +20,12 @@ CREATE TABLE `wpui_usermeta` (
   PRIMARY KEY (`umeta_id`),
   KEY `user_id` (`user_id`),
   KEY `meta_key` (`meta_key`(191))
-) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wpui_usermeta` WRITE;
 /*!40000 ALTER TABLE `wpui_usermeta` DISABLE KEYS */;
-INSERT INTO `wpui_usermeta` VALUES (1,1,'nickname','nstudios'),(2,1,'first_name',''),(3,1,'last_name',''),(4,1,'description',''),(5,1,'rich_editing','true'),(6,1,'comment_shortcuts','false'),(7,1,'admin_color','fresh'),(8,1,'use_ssl','0'),(9,1,'show_admin_bar_front','true'),(10,1,'wpui_capabilities','a:1:{s:13:\"administrator\";b:1;}'),(11,1,'wpui_user_level','10'),(12,1,'dismissed_wp_pointers',''),(13,1,'show_welcome_panel','1'),(14,1,'session_tokens','a:1:{s:64:\"cdeae353cb26e707720e92cd67fb3e2366364ab2e41d044847966010a2e0bfa9\";a:4:{s:10:\"expiration\";i:1465507188;s:2:\"ip\";s:13:\"50.148.233.78\";s:2:\"ua\";s:109:\"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36\";s:5:\"login\";i:1465334388;}}'),(15,1,'wpui_user-settings','libraryContent=browse'),(16,1,'wpui_user-settings-time','1465334383'),(17,1,'wpui_dashboard_quick_press_last_post_id','3');
+INSERT INTO `wpui_usermeta` VALUES (1,1,'nickname','nstudios'),(2,1,'first_name',''),(3,1,'last_name',''),(4,1,'description',''),(5,1,'rich_editing','true'),(6,1,'comment_shortcuts','false'),(7,1,'admin_color','fresh'),(8,1,'use_ssl','0'),(9,1,'show_admin_bar_front','true'),(10,1,'wpui_capabilities','a:1:{s:13:\"administrator\";b:1;}'),(11,1,'wpui_user_level','10'),(12,1,'dismissed_wp_pointers',''),(13,1,'show_welcome_panel','1'),(14,1,'session_tokens','a:1:{s:64:\"f158cf874528a024425b04bc89231288961961a525ec12dbd9396da2891be919\";a:4:{s:10:\"expiration\";i:1465853958;s:2:\"ip\";s:13:\"50.148.233.78\";s:2:\"ua\";s:108:\"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36\";s:5:\"login\";i:1465681158;}}'),(15,1,'wpui_user-settings','libraryContent=browse'),(16,1,'wpui_user-settings-time','1465334383'),(17,1,'wpui_dashboard_quick_press_last_post_id','3'),(18,1,'managenav-menuscolumnshidden','a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}'),(19,1,'metaboxhidden_nav-menus','a:10:{i:0;s:19:\"add-post-type-agent\";i:1;s:22:\"add-post-type-property\";i:2;s:22:\"add-post-type-partners\";i:3;s:19:\"add-post-type-slide\";i:4;s:12:\"add-post_tag\";i:5;s:15:\"add-post_format\";i:6;s:20:\"add-property-feature\";i:7;s:17:\"add-property-type\";i:8;s:17:\"add-property-city\";i:9;s:19:\"add-property-status\";}');
 /*!40000 ALTER TABLE `wpui_usermeta` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
