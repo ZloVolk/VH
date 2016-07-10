8207c0aa4a3d568812a01f74f4b842e73f73e3a4
nstudios <m.godfrey@33d.us>
2016-07-09 19:02:33 -0500

    Vinehurst Release 3.4

--- wp-content/plugins/seedprod-coming-soon-pro/SeedReduxCore/languages/README.txt
+++
@@ -1,1 +0,0 @@
-Please visit: https://github.com/SeedReduxFramework/SeedReduxFramework/wiki/Translate for details on how you can help.
--- wp-content/uploads/revisr-backups/revisr_wpqp_mk_components.sql
+++ wp-content/uploads/revisr-backups/revisr_wpqp_mk_components.sql
@@ -1,4 +1,17 @@
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 DROP TABLE IF EXISTS `wpqp_mk_components`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wpqp_mk_components` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -10,7 +23,20 @@ CREATE TABLE `wpqp_mk_components` (
   KEY `wpqp_mk_components_type` (`type`),
   KEY `wpqp_mk_components_status` (`status`),
   KEY `wpqp_mk_components_name` (`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=7076 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10276 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 LOCK TABLES `wpqp_mk_components` WRITE;
-INSERT INTO `wpqp_mk_components` VALUES ('7075','short_code','1','mk_dropcaps','2016-06-30 23:22:21','2016-06-30 23:22:21'), ('7074','short_code','1','mk_gallery','2016-06-30 23:22:21','2016-06-30 23:22:21'), ('7073','short_code','1','mk_icon_box_gradient','2016-06-30 23:21:25','2016-06-30 23:21:25'), ('7072','short_code','1','mk_lcd_slideshow','2016-06-30 23:21:25','2016-06-30 23:21:25'), ('7071','short_code','1','mk_icon_box','2016-06-30 23:21:25','2016-06-30 23:21:25'), ('7070','short_code','1','mk_testimonials','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7069','short_code','1','mk_employees','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7068','short_code','1','mk_font_icons','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7067','short_code','1','mk_portfolio','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7066','short_code','1','mk_icon_box2','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7065','short_code','1','mk_custom_box','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7064','short_code','1','vc_row_inner','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7063','short_code','1','mk_image','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7062','short_code','1','mk_divider','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7061','short_code','1','vc_row','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7060','short_code','1','mk_button_gradient','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7059','short_code','1','vc_column_text','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7058','short_code','1','mk_fancy_title','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7057','short_code','1','mk_padding_divider','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7056','short_code','1','vc_column','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7055','short_code','1','mk_page_section','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7054','short_code','1','mk_swipe_slideshow','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7053','short_code','1','mk_audio','2016-06-30 23:18:00','2016-06-30 23:18:00'), ('7052','short_code','1','mk_button','2016-06-30 23:18:00','2016-06-30 23:18:00');
+/*!40000 ALTER TABLE `wpqp_mk_components` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wpqp_mk_components` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
